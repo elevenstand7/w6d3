26be3713d94d1e2073e9dbe3c4b148be4fe5d95c
elevenstand7 <elevenstand7@gmail.com>
2023-08-02 15:37:41 -0400

    up to ph3

--- RoutesProject/app/controllers/users_controller.rb
+++ RoutesProject/app/controllers/users_controller.rb
@@ -2,4 +2,13 @@ class UsersController < ApplicationController
   def index
      render plain: "I am in the index action!"
   end
+
+
+  def create
+    render json: params
+  end
+
+  def show
+    render json: params[:id]
+  end
 end
