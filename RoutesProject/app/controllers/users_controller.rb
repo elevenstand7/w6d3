f65da58675c1603c66bb34c81c468cbabc0481c6
Naim Miah <mmiah@fordham.edu>
2023-08-02 16:28:37 -0400

    completed classwork

--- RoutesProject/app/controllers/users_controller.rb
+++ RoutesProject/app/controllers/users_controller.rb
@@ -1,12 +1,11 @@
 class UsersController < ApplicationController
   def index
-     render plain: "I am in the index action!"
+    render plain: "I am in the index action!"
   end
-
 
   def create
     user = User.new(params.require(:user).permit(:name, :email))
-  # replace the `user_attributes_here` with the actual attribute keys
+    # replace the `user_attributes_here` with the actual attribute keys
     if user.save
       render json: user
     else
@@ -19,5 +18,20 @@ class UsersController < ApplicationController
     render json: user
   end
 
+  def update
+    user = User.find(params[:id])
+    user.update!(user_params)
+    render json: user
+  end
 
+  def destroy
+    User.destroy(params[:id])
+
+    redirect_to controller: :users, action: :index
+  end
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
